db83c97ea2085127e002ad1125fcec1d23e3a8c3
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-20 16:30:14 +0500

    changes

--- my_project/src/App.jsx
+++ my_project/src/App.jsx
@@ -1,11 +1,24 @@
 import Total_members from "./components/Total_members"
+import Cards_Faith from "./components/cards_Faith"
 
 function App() {
   
 
   return (
     <>
-    <div className="flex">
+    <h1 className="text-5xl font-bold pt-40 pl-64 ">Faith</h1>
+    <div className="flex items-center justify-center pb-28">
+    <div className="pt-30 pl-30">
+    <Cards_Faith/>
+    </div>
+    <div className="pt-30 pl-30">
+    <Cards_Faith/>
+    </div>
+    <div className="pt-30 pl-30">
+    <Cards_Faith/>
+    </div>
+    </div>
+    {/* <div className="flex">
 
     <div className="h-28 w-1/4 ">
       <Total_members value={5666} title="total members" />
@@ -19,7 +32,7 @@ function App() {
     <div className="h-28 w-1/4 ml-7">
       <Total_members value={5666} title="tserver load"  />
     </div>
-    </div>
+    </div> */}
      </>
   )
 }
