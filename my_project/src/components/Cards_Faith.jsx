51e06b60a3467b1f45e4a3b6f4c7380fc99cf0b8
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-21 10:15:42 +0500

    changes

--- my_project/src/components/Cards_Faith.jsx
+++ my_project/src/components/Cards_Faith.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 export default function Cards_Faith(props) {
   return (
     <div className='bg-white shadow-xl/30 p-4 rounded-2xl w-60 h-80'>
         <div className='flex justify-center relative -top-14'><img className='size-32' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ1uW4bSv_SXJE9XOAchn_XfOJ3iLJzLbkEQ&s" alt="Image of person" /></div>
-        <h1 className='text-2xl font-bold flex-wrap pt-20'>{props.text}</h1>
+        <h1 className='text-2xl font-bold flex-wrap pt-24'>{props.text}</h1>
     </div>
   )
 }
